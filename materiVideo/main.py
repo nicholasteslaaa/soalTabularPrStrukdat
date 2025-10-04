5b458efb3ce74b3f8d418a1e2161ffe8745ff54e
Nicsap2005 <143601631+nicholasteslaaa@users.noreply.github.com>
2025-10-04 18:36:42 +0700

    udh selesai video materinya

--- materiVideo/main.py
+++ materiVideo/main.py
@@ -1,98 +1,70 @@
 import pandas as pd
 from datetime import datetime
+import numpy as np
 
 class excelManager:
     def __init__(self,path:str,sheetName:str = "Sheet1",primaryKey = "ID"):
         self.path = path
         self.sheetName = sheetName
-        self.primaryKey = primaryKey
         self.df = pd.read_excel(path)
-        
-    def insertData(self,newData:dict):
-        collumn = self.df.columns
-        new_row = {}
-        
-        checkIfExist = self.getData(self.primaryKey,newData[self.primaryKey])
-        if (checkIfExist): return {"status":"error","message":"Nim already exist"}
+        self.primaryKey = None
+        for i in self.df.columns:
+            if (i.strip().lower() == primaryKey.strip().lower()):
+                self.primaryKey = i
             
+    def insertData(self,newData:dict):
+        new_row = {}
+        checkIfExist = self.getData(self.primaryKey,newData[self.primaryKey])
+        
+        if (checkIfExist): return None
         for newValue in newData:
-            for col in collumn:
+            for col in self.df.columns:
                 if (str(newValue).lower() == str(col).lower()):
                     new_row.update({col:newData[newValue]})
                     break
-        
         # Append row
-        self.df = pd.concat([self.df, pd.DataFrame([new_row])], ignore_index=True)
+        self.df = pd.concat([self.df, pd.DataFrame([new_row])], ignore_index=True)        
+    
+    def deleteData(self,targetedID):
+        isExist = self.getData(self.primaryKey,targetedID)
+        if (not isExist): return None
         
-        return {"status":"success","message":"inserted"}
-        
-        
-    def deleteData(self,targetedID:str):
-        target = self.getData(self.primaryKey,targetedID)
-        
-        if (not target): return {"status":"error","message":"Nim Not Found"}
-        
-        self.df.drop(target["Row"], inplace=True)
-        
-        return {"status":"success","message":"Deleted"}
-                
+        self.df.drop(isExist["row"], inplace=True)
         
     def editData(self,targetedId:str, newData:dict) -> dict:
-        targetData = self.getData(self.primaryKey,targetedId)
+        isExist = self.getData(self.primaryKey,targetedId)
+        if (not isExist): return None
         
-        if (not targetData): return {"status":"error","message":"Nim Not Found"}
+        for key in newData:
+            self.df.at[isExist["row"],key] = newData[key]
 
-        for inputKey in newData:
-            for colName in self.df.columns:
-                if (str(inputKey).lower() == str(colName).lower()):
-                    self.df.at[targetData["Row"],colName] = newData[inputKey]
-                    break
-        
-        return {"status":"success","message":"edited"}
-
-    def getData(self,colName:str, data:str) -> dict:
-        collumn = self.df.columns # mendapatkan list dari nama kolom tabel
-        
-        # cari index dari nama kolom dan menjaganya dari typo atau spasi berlebih
-        collumnIndex = [i for i in range(len(collumn)) if (collumn[i].lower().strip() == colName.lower().strip())] 
-        
-        # validasi jika input kolom tidak ada pada data excel
-        if (len(collumnIndex) != 1): return None
-        
-        # nama kolom yang sudah pasti benar dan ada
-        colName = collumn[collumnIndex[0]]
-        
-        
-        resultDict = dict() # tempat untuk hasil
-        
-        for i in self.df.index: # perulangan ke baris tabel
-            cellData = str(self.df.at[i,colName]) # isi tabel yand dijadikan str
-            if (cellData == data): # jika data cell sama dengan data input
-                for col in collumn: # perulangan ke nama-nama kolom
-                    resultDict.update({str(col):str(self.df.at[i,col])}) # masukan data {namaKolom : data pada cell} ke resultDict
-                resultDict.update({"Row":i}) # tambahkan row nya pada resultDict
-                return resultDict # kembalikan resultDict
-        
-        return None
+    def getData(self,colName:str, data) -> dict:
+        data = str(data)
+        for row in self.df.index:
+            temp = {}
+            for col in self.df.columns:
+                temp.update({col:self.df.at[row,col]})
+            temp.update({"row":row})
+            if (str(temp[colName]).lower().strip() == data):
+                return {"result":temp,"row":row}
+            
     
     def saveChange(self):
-        self.df.to_excel(self.path, sheet_name=self.sheetName , index=False)
+        self.df.to_excel(self.path, sheet_name=self.sheetName, index=False)
 
 
-dataBarang = excelManager("materiVideo/dataBarangMinimarket.xlsx")
+dataBarang = excelManager("materiVideo/dataBarangMinimarket.xlsx",primaryKey="ID")
 dataPenjualan = excelManager("materiVideo/dataPenjualanMinimarket.xlsx",primaryKey="IDPejualan")
 
 def jual(idBarang,jumlahBarang):
-    data = dataBarang.getData("ID",idBarang)
-    print(data)
-    
+    data = dataBarang.getData("ID",idBarang)["result"]
+
     if (data == None): return f"ID: {idBarang} tidak di temukan"
     if (int(data["Stok"])-jumlahBarang < 0): return "jumlah barang melebihi stok barang"
-        
+
     dataPenjualan.insertData({"IDPejualan":str(datetime.now().strftime("%Y%m%d%H%M%S%f")),"ID" : data["ID"],"Kategori":data["Kategori"],"Harga":data["Harga"],"Waktu":datetime.now(),"Jumlah barang":jumlahBarang,"Total":float(data["Harga"])*jumlahBarang})
     dataPenjualan.saveChange()
-    dataBarang.editData(idBarang,{"ID":data["ID"],"Nama":data["Nama"],"Perusahaan Asal":data["Perusahaan Asal"],"Kategori":data["Kategori"],"Harga":data["Harga"],"Stok":int(data["Stok"])-jumlahBarang})
-
+    dataBarang.editData(idBarang,{"ID":data["ID"],"Nama":data["Nama"],"Perusahaan Asal":data["Perusahaan Asal"],"Kategori":data["Kategori"],"Harga":data["Harga"],"Stok":data["Stok"]-jumlahBarang})
 
 def restock():
     pass
@@ -101,6 +73,6 @@ def barangPalingLaku():
     pass
 
 
-jual("8885193814391",139)
+jual(8885193814391,139)
 print(dataPenjualan.df.head(10)) 
 print(dataBarang.df.head())
