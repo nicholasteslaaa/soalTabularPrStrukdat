c3ef6d415168883f4e1cb7f8d2eb670e7f44c0c9
Nicsap2005 <143601631+nicholasteslaaa@users.noreply.github.com>
2025-10-04 13:03:38 +0700

    update video materi

--- materiVideo/main.py
+++ materiVideo/main.py
@@ -2,14 +2,17 @@ import pandas as pd
 from datetime import datetime
 
 class excelManager:
-    def __init__(self,path:str):
+    def __init__(self,path:str,sheetName:str = "Sheet1",primaryKey = "ID"):
+        self.path = path
+        self.sheetName = sheetName
+        self.primaryKey = primaryKey
         self.df = pd.read_excel(path)
         
     def insertData(self,newData:dict):
         collumn = self.df.columns
         new_row = {}
         
-        checkIfExist = self.getData("ID",newData["ID"])
+        checkIfExist = self.getData(self.primaryKey,newData[self.primaryKey])
         if (checkIfExist): return {"status":"error","message":"Nim already exist"}
             
         for newValue in newData:
@@ -24,8 +27,8 @@ class excelManager:
         return {"status":"success","message":"inserted"}
         
         
-    def deleteData(self,targetedNim:str):
-        target = self.getData("NIM",targetedNim)
+    def deleteData(self,targetedID:str):
+        target = self.getData(self.primaryKey,targetedID)
         
         if (not target): return {"status":"error","message":"Nim Not Found"}
         
@@ -35,7 +38,7 @@ class excelManager:
                 
         
     def editData(self,targetedId:str, newData:dict) -> dict:
-        targetData = self.getData("ID",targetedId)
+        targetData = self.getData(self.primaryKey,targetedId)
         
         if (not targetData): return {"status":"error","message":"Nim Not Found"}
 
@@ -71,14 +74,14 @@ class excelManager:
                 return resultDict # kembalikan resultDict
         
         return None
-
+    
+    def saveChange(self):
+        self.df.to_excel(self.path, sheet_name=self.sheetName , index=False)
 
 
 dataBarang = excelManager("materiVideo/dataBarangMinimarket.xlsx")
-dataPenjualan = excelManager("materiVideo/dataPenjualanMinimarket.xlsx")
+dataPenjualan = excelManager("materiVideo/dataPenjualanMinimarket.xlsx",primaryKey="IDPejualan")
 
-print(dataPenjualan.df.columns)
-print(dataBarang.df.head())
 def jual(idBarang,jumlahBarang):
     data = dataBarang.getData("ID",idBarang)
     print(data)
@@ -86,15 +89,17 @@ def jual(idBarang,jumlahBarang):
     if (data == None): return f"ID: {idBarang} tidak di temukan"
     if (int(data["Stok"])-jumlahBarang < 0): return "jumlah barang melebihi stok barang"
         
-    dataPenjualan.insertData({"ID" : data["ID"],"Kategori":data["Kategori"],"Harga":data["Harga"],"Waktu":datetime.now(),"Jumlah barang":jumlahBarang,"Total":float(data["Harga"])*jumlahBarang})
-    # print(float(data["Stok"])-jumlahBarang)
+    dataPenjualan.insertData({"IDPejualan":str(datetime.now().strftime("%Y%m%d%H%M%S%f")),"ID" : data["ID"],"Kategori":data["Kategori"],"Harga":data["Harga"],"Waktu":datetime.now(),"Jumlah barang":jumlahBarang,"Total":float(data["Harga"])*jumlahBarang})
+    dataPenjualan.saveChange()
     dataBarang.editData(idBarang,{"ID":data["ID"],"Nama":data["Nama"],"Perusahaan Asal":data["Perusahaan Asal"],"Kategori":data["Kategori"],"Harga":data["Harga"],"Stok":int(data["Stok"])-jumlahBarang})
 
-jual("8885193814391",140)
-print(dataPenjualan.df.head()) 
-print(dataBarang.df.head())
+def restock():
+    pass
 
-# m.insertData({'ID': '101', 'Nama': 'Saori Saus Tiram', 'Perusahaan Asal': 'Wings', 'Kategori': 'Bumbu Dapur', 'Harga': 40020, 'Row': 0})
-# # print(getData("ID","101"))
-# m.df.sort_values(by="Harga",ascending=False)
-# print(m.df.head(200))
+def barangPalingLaku():
+    pass
+
+
+jual("8885193814391",139)
+print(dataPenjualan.df.head(10)) 
+print(dataBarang.df.head())
